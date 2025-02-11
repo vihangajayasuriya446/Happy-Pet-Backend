47b7925bf735638712f1ddcfb852d93d29a0beec
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-02-11 23:32:42 +0530

    added backend layers

---
+++ happypet/src/main/java/com/example/happypet/controller/UserController.java
@@ -0,0 +1,53 @@
+package com.example.happypet.controller;
+
+import com.example.happypet.dto.UserDTO;
+import com.example.happypet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping(value = "api/v1/")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/getusers")
+    public List<UserDTO> getUsers(){
+        return userService.getAllUsers();
+    }
+
+    @PostMapping("/adduser")
+    public UserDTO saveUser(@RequestParam("id") int id,
+                            @RequestParam("name") String name,
+                            @RequestParam("type") String type,
+                            @RequestParam("age") int age,
+                            @RequestParam("gender") String gender,
+                            @RequestParam("breed") String breed,
+                            @RequestParam("location") String location,
+                            @RequestParam("photo") MultipartFile photo)throws IOException {
+        return userService.saveUser(id, name, type, age, gender, breed, location, photo);
+    }
+
+    @PutMapping("/updateuser")
+    public UserDTO updateUser(@RequestParam("id") int id,
+                              @RequestParam("name") String name,
+                              @RequestParam("type") String type,
+                              @RequestParam("age") int age,
+                              @RequestParam("gender") String gender,
+                              @RequestParam("breed") String breed,
+                              @RequestParam("location") String location,
+                              @RequestParam(value = "photo", required = false) MultipartFile photo) throws IOException {
+        return userService.updateUser(id, name, type, age, gender, breed, location, photo);
+    }
+
+    @DeleteMapping("/deleteuser/{userId}")
+    public void deleteUser(@PathVariable Integer userId){
+        userService.deleteUser(userId);
+    }
+
+}
---
+++ happypet/src/main/java/com/example/happypet/dto/UserDTO.java
@@ -0,0 +1,22 @@
+package com.example.happypet.dto;
+
+import jakarta.persistence.Lob;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserDTO {
+    private int id;
+    private String name;
+    private String type;
+    private int age;
+    private String gender;
+    private String breed;
+    private String location;
+
+    private String photo;
+}
