7cb0cddc01d6a66ce51c76cfbeebd31f5e3e79a6
Hasindu0 <20222125.hasindu@iit.ac.lk>
2025-03-05 23:37:12 +0530

    Commit

--- happypet/src/main/java/com/example/happypet/repo/OwnerRepo.java
+++ happypet/src/main/java/com/example/happypet/repo/OwnerRepo.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OwnerRepo extends JpaRepository<Owner, Integer> {
-    // You can add custom query methods here if needed
+
 }
