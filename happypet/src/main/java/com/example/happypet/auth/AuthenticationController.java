320837b7718d2349b3ac83701e4212d3847671a2
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-03-07 11:33:02 +0530

    Added auth layer in spring security

---
+++ happypet/src/main/java/com/example/happypet/auth/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.example.happypet.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        System.out.println("Yes");
+        return ResponseEntity.status(HttpStatus.OK).body(authenticationService.register(request));
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return ResponseEntity.status(HttpStatus.OK).body(authenticationService.authenticate(request));
+    }
+}
