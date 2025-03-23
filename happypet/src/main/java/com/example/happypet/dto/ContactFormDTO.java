9e60f85c27b1f50fbbc0f7ac5395e9efc8fc2503
GeethalPeiris <geethal23310@gmail.com>
2025-03-23 23:40:25 +0530

    added DTO

---
+++ happypet/src/main/java/com/example/happypet/dto/ContactFormDTO.java
@@ -0,0 +1,12 @@
+package com.example.happypet.dto;
+
+import lombok.Data;
+
+@Data
+public class ContactFormDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String subject;
+    private String message;
+}
