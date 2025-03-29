198999a1e9a0fafc7b1b838eeb8284b65b1fcaf6
vihangajayasuriya446 <vxhaaaaa@gmail.com>
2025-03-29 22:59:55 +0530

    Unwanted files cleared

--- happypet/src/main/java/com/example/happypet/repo/BuyerRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.happypet.repo;
-
-import com.example.happypet.model.Buyer;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BuyerRepo extends JpaRepository<Buyer, Integer> {
-}
--- happypet/src/main/java/com/example/happypet/repo/PetStoreRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.happypet.repo;
-
-import com.example.happypet.model.PetStore;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PetStoreRepo extends JpaRepository<PetStore, Integer> {
-}
